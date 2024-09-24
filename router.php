a3c10f349c531df7aae63b6cd1cc86b02ba56d55
Arthur Hofmann <arthur.hofmann89@gmail.com>
2024-09-24 19:40:27 +0200

    Update - class Route programmed

--- router.php
+++ router.php
@@ -2,37 +2,35 @@
 
 class Router{
 
-  private $routes = [];
-
-
+  private $routes =[];
   public function add($method, $path, $controllerAction){
     $this->routes[$method][$path] = $controllerAction;
   }
 
-
-  public function dispatch(){
+  public function dispatach(){
     $method = $_SERVER["REQUEST_METHOD"];
     $path = $_SERVER["REQUEST_URI"];
-
-    foreach ($this->routes[$method] ?? [] as $route => $controllerAction) {
+  
+    foreach($this->routes[$method] ?? [] as $route => $controllerAction){
+      
       if(preg_match($this->formatRoute($route), $path, $matches)){
         array_shift($matches);
         list($class, $action) = explode("@", $controllerAction);
+        
         if(class_exists($class) && method_exists($class, $action)){
           return call_user_func_array([new $class, $action], $matches);
         }
       }
-    }
 
-    http_response_code(404);
-    echo "404 Not Found";
+      http_response_code(404);
+      echo "404 Not Found";
+
+
+    }
   }
 
-
-
-  private function formatRoute($route){
+  public function formatRoute($route){
     $route = preg_replace('/\{([^\/]+)\}/', '(?P<$1>[^/]+)', $route);
     return "#^$route$#";
   }
-
 }
