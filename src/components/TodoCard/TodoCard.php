f5ee84f1a9e5fee45344addf1757b01622219824
Arthur Hofmann <arthur.hofmann89@gmail.com>
2024-09-25 10:20:33 +0200

    Update - new dir and file

---
+++ src/components/TodoCard/TodoCard.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\components\TodoCard;
+
+class TodoCard{
+  public function createCard(){
+    
+  }
+}
--- src/views/home.php
+++ src/views/home.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="src/views/home.css">
   <title>Home View</title>
 </head>
+
 <body>
   <h1>
     <?php
-      echo $message
+    echo $message
     ?>
   </h1>
+
+  <div class="card">
+    <div class="cardHead">
+      <h3>Todo Title</h3>
+    </div>
+    <div class="cardBody">
+      <p>Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit.
+        Consequuntur saepe repudiandae non?
+        Nostrum perferendis magnam excepturi
+        hic perspiciatis. Quo, mollitia?</p>
+    </div>
+    <div class="cardFooter">
+      <small>Erledigt</small>
+      <button></button>
+    </div>
+  </div>
 </body>
+
 </html>
