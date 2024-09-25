1d35a47fe77e280b31b62d7d98087d41f7c24ca3
Arthur Hofmann <arthur.hofmann89@gmail.com>
2024-09-25 08:31:49 +0200

    Update - index.php and Controllers

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
   require_once("Router.php");
   require_once("src/controllers/HomeController.php");
-  require_once("src/controllers/AboutController.php");
-  require_once("src/controllers/BooksController.php");
   
 
   $router = new Router();
 
   $router->add("GET", "/PHP_ToDo_Testapp/", "HomeController@index");
-  $router->add("GET", "/PHP_ToDo_Testapp/about", "AboutController@index");
-  $router->add("GET", "/PHP_ToDo_Testapp/books/{id}", "BooksController@index");
   
 
   $router->dispatch();
